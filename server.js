ea013f57b051cd442b8ca1cc6a4b7ed750688b08
Santino Petrovic <santinopetrovic@gmail.com>
2019-02-16 10:26:42 +0000

    Require sqlite3 node module and 'booting' up data.db file.

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const passport = require('passport');
+const sqlite3 = require('sqlite3');
+
+// Getting other js files
+const config = require('./config/database');
 
 const app = express();
 
@@ -15,10 +20,17 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// "Booting" up DB
+const DB = new sqlite3.Database(config.dbpath, function(err){
+    if (err) {
+        return err;
+    }
+});
+
 app.get('/', function (req, res) {
-  res.send('Invalid Endpoint.');
+    res.send('Invalid Endpoint.');
 })
 
 app.listen(port, function () {
-  console.log('Server is running on port 3000.')
+    console.log('Server is running on port 3000.');	  
 })
