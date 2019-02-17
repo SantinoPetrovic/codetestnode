0204ce956f5f97de2361ed8a02a79dda11a2b9a2
Santino Petrovic <santinopetrovic@gmail.com>
2019-02-17 12:42:24 +0000

    added device component and alarm component.

--- my-app-axis/src/app/app.module.ts
+++ my-app-axis/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SitesComponent } from './components/sites/sites.component';
 import { LoginComponent } from './components/login/login.component';
+import { AlarmComponent } from './components/alarm/alarm.component';
+import { DeviceComponent } from './components/device/device.component';
+
+const appRoutes: Routes = [
+ {path:'', component: HomeComponent},
+ {path:'users', component: UsersComponent},
+ {path:'sites', component: SitesComponent},
+ {path:'sites/:siteid/alarmZones', component: AlarmComponent},
+ {path:'sites/:siteid/devices', component: DeviceComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SitesComponent,
-    LoginComponent
+    LoginComponent,
+    AlarmComponent,
+    DeviceComponent
   ],
   imports: [
     BrowserModule,
